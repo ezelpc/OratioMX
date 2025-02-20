fc9263da065933e71b9e76f8ee7c4183b7e35508
ezelpc <ezequielpc496@gmail.com>
2025-02-20 11:15:14 +0000

    Creacion del main.py y actualizacin de anexos

---
+++ Backend/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from app.routes.api import router as api_router
+from app.routes.websocket import router as ws_router
+
+app = FastAPI(Title="Oratio MX API")
+
+app.include_router(api_router, prefix="/api")
+app.include_router(ws_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
