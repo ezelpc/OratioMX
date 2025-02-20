0518e0988f93e286b0d16db23f31483796d28e8f
ezelpc <ezequielpc496@gmail.com>
2025-02-20 11:21:37 +0000

    correccion de api

--- Backend/app/routes/api.py
+++ Backend/app/routes/api.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter, UploadFile, File
 from app.models.mobilenet import signLanguageModel
+import os
 
 router = APIRouter()
 model = signLanguageModel()
 
 @router.post("/predict")
 async def predict_image(file: UploadFile = File(...)):
+    # Asegúrate de que la carpeta 'images' exista
+    os.makedirs("images", exist_ok=True)
+    
     image_path = f"images/{file.filename}"
 
     with open(image_path, "wb") as buffer:
